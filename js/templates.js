0b1f9f3c9d4f0fe688d33497ad1a20032fd384a1
Dominic Myers <annoyingmouse@gmail.com>
2014-01-02 15:35:55 +0000

    Update templates.js

--- js/templates.js
+++ js/templates.js
@@ -5,7 +5,7 @@ $.templates({
                 '<h2><a id="{{>id}}">{{>title}}</a></h2>' +
                 '<p>Published: <time datetime="{{>pubdate}}">{{>date}}</time></p>' +
             '</header>' +
-            {{:content}}' +
+            '{{:content}}' +
         '</article>' +
         '<hr />'
 });
